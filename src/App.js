8bbeb40e406cf693bd15597d05d78d871a9e7354
EstDua <92175044+EstDua@users.noreply.github.com>
2021-12-18 18:48:07 +0100

    nuevo1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PaginaPrincipal from './Components/PaginaPrincipal';
-import BotonesPrincipales from './Components/BotonesPaginaPrincipal'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from './Components/Inicio/Layout';
+import Registro from './Components/Registro/Registro'
+import Sesion from './Components/Sesión/Sesion'
+import DashAdmin from './Components/DashAdmin/Layout'
+import DashCli from './Components/DashCli/Layout'
+import DashEmp from './Components/DashEmploy/Layout'
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Routes>
 
-function App() {
-  return (
-    <BrowserRouter>
-    <PaginaPrincipal/>
-    <BotonesPrincipales/>
-    </BrowserRouter>
-  );
+            <Route path={'/'} element={<Inicio />} />
+            <Route path={'/registro'} element={<Registro />} />
+            <Route path={'/sesion'} element={<Sesion />} />
+            <Route path={'/dashadmin'} element={<DashAdmin />} />
+            <Route path={'/dashcli'} element={<DashCli/>} />
+            <Route path={'/dashemp'} element={<DashEmp/>} />
+            
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as serviceWorker from './serviceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from './Components/Inicio/Layout';
+import Registro from './Components/Registro/Registro';
 
-// let App = function myComponente() {
-//     return (
-//     <div>
-//         <h1>Hola Mundo, grupo de programacion</h1>
-//     </div>
-//     )
-// }
+ReactDOM.render(
+  
 
+     <App />,
+  document.getElementById('root')
+);
 
-
-ReactDOM.render(<App/>, document.getElementById('root'))
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorker.unregister();
